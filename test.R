96d8f4c5a68095fb9ec6b06858d41aa9801da743
zhihuai1982 <zhihuai1982@gmail.com>
2020-04-27 00:15:12 +0800

    add ncm2

--- test.R
+++ test.R
@@ -3,3 +3,16 @@ data:q
 fjd
 library(dplyr)
 data.frame(1:3)
+    data.entry  config.json
+config.json config.jsonj
+\README.mf
+diso
+data.entry
+dimnames.data.frameames<-.dataamerfkf
+config.jsonj
+date
+        jitterj：itter
+jitterj：itterr
+.vimrc
+:q
+
