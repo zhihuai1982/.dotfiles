3633697706fa17da720d5853e929924c65a4c532
zhihuai1982 <zhihuai1982@zhihuai1982deiMac.local>
2020-06-01 19:24:12 +0800

    hackmac time sync

---
+++ time_sync.sh
@@ -0,0 +1,7 @@
+
+# sudo sntp -sS time.euro.apple.com
+# osascript -e 'tell application "Terminal" do shell script "sudo sntp
+# ntp.aliyun.com" end tell'
+# sudo sntp -sS ntp.aliyun.com
+osascript -e 'tell application "time_sync" to activate'
+
