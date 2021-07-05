60d83c6c1cf917e8feeea2d491389dbf57df6787
“zhihuai1982” <zhihuai1982@gmail.com>
2021-07-05 22:28:23 +0800

    SyncDate 2021-07-05

--- gitsync.sh
+++ gitsync.sh
@@ -3,7 +3,9 @@
 cd /home/zhihuai1982/Media14T/nsrr/Sleeposition
 git add .
 git commit -m "SyncDate `date '+%F'`"
+git push
 
 cd /home/zhihuai1982/.dotfiles
 git add .
 git commit -m "SyncDate `date '+%F'`"
+git push
