e83c9d4a2e90b11690186ce3227a5d302d99d421
“zhihuai1982” <zhihuai1982@gmail.com>
2022-02-12 09:12:39 +0800

    papaja template

--- gitsync.sh
+++ gitsync.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# crontab -e
+# then add
+# 20 1 * * * ~/.dotfiles/gitsync.sh 2>autogitpush.err
+
 cd /home/zhihuai1982/Documents/OSA_with_obesity
 git add .
 git commit -m "SyncDate `date '+%F'`"
