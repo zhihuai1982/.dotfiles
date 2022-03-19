be2e73620428165a70a485ccdfe23b16a45f76f9
zhihuai1982 <zhihuai1982@gmail.com>
2022-03-19 15:05:23 +0800

    window manage updated

--- .hammerspoon/key-binding.lua
+++ .hammerspoon/key-binding.lua
@@ -35,28 +35,28 @@ windowBind({"alt", "ctrl"}, {
 })
 
 -- * Set Window Position on screen
-windowBind({"ctrl", "alt"}, {
-  y = wm.rightToLeft,      -- ⌃⌥⇧ + ←
-  o = wm.rightToRight,    -- ⌃⌥⇧ + →
-  i = wm.bottomUp,           -- ⌃⌥⇧ + ↑
-  u = wm.bottomDown        -- ⌃⌥⇧ + ↓
+--windowBind({"ctrl", "alt"}, {
+  --y = wm.rightToLeft,      -- ⌃⌥⇧ + ←
+  --o = wm.rightToRight,    -- ⌃⌥⇧ + →
+  --i = wm.bottomUp,           -- ⌃⌥⇧ + ↑
+  --u = wm.bottomDown        -- ⌃⌥⇧ + ↓
   --left = wm.rightToLeft,      -- ⌃⌥⇧ + ←
   --right = wm.rightToRight,    -- ⌃⌥⇧ + →
   --up = wm.bottomUp,           -- ⌃⌥⇧ + ↑
   --down = wm.bottomDown        -- ⌃⌥⇧ + ↓
-})
+--})
 
 -- * Set Window Position on screen
-windowBind({"ctrl", "alt"}, {
-  n = wm.leftToLeft,      -- ⌥⌘⇧ + ←
-  ["."] = wm.leftToRight,    -- ⌥⌘⇧ + →
-  [","] = wm.topUp,             -- ⌥⌘⇧ + ↑
-  m = wm.topDown          -- ⌥⌘⇧ + ↓
+--windowBind({"ctrl", "alt"}, {
+  --n = wm.leftToLeft,      -- ⌥⌘⇧ + ←
+  --["."] = wm.leftToRight,    -- ⌥⌘⇧ + →
+  --[","] = wm.topUp,             -- ⌥⌘⇧ + ↑
+  --m = wm.topDown          -- ⌥⌘⇧ + ↓
   --left = wm.leftToLeft,      -- ⌥⌘⇧ + ←
   --right = wm.leftToRight,    -- ⌥⌘⇧ + →
   --up = wm.topUp,             -- ⌥⌘⇧ + ↑
   --down = wm.topDown          -- ⌥⌘⇧ + ↓
-})
+--})
 
 ---- * Windows-like cycle
 --windowBind({"ctrl", "alt"}, {
@@ -64,8 +64,10 @@ windowBind({"ctrl", "alt"}, {
   --i = wm.cycleRight          -- ⌃⌥⌘ + i
 --})
 
+
+--folk from https://github.com/sugood/hammerspoon
 -- window management
---local application = require "hs.application"
+local application = require "hs.application"
 local hotkey = require "hs.hotkey"
 local window = require "hs.window"
 local screen = require "hs.screen"
@@ -79,10 +81,92 @@ local mouse = require "hs.mouse"
 --end)
 
  --move cursor to next monitor
---hotkey.bind({"alt","ctrl"}, "i", function ()
-  --focusScreen(window.focusedWindow():screen():next())
+hotkey.bind({"alt","ctrl"}, "i", function ()
+  focusScreen(window.focusedWindow():screen():next())
+end)
+
+-- top half
+hotkey.bind({"ctrl","alt"}, "p", function()
+  window.focusedWindow():moveToUnit'[0,0,100,50]'
+end)
+
+---- bottom half
+hotkey.bind({"ctrl","alt"}, "/", function()
+  window.focusedWindow():moveToUnit'[0,50,100,100]'
+end)
+
+-- left top quarter
+hotkey.bind({"ctrl","alt"}, "-", function()
+  window.focusedWindow():moveToUnit'[0,0,50,50]'
+end)
+
+---- right bottom quarter
+hotkey.bind({"ctrl","alt"}, "]", function()
+  window.focusedWindow():moveToUnit'[50,50,100,100]'
+end)
+
+---- right top quarter
+hotkey.bind({"ctrl","alt"}, "=", function()
+  window.focusedWindow():moveToUnit'[50,0,100,50]'
+end)
+
+---- left bottom quarter
+hotkey.bind({"ctrl","alt"}, "[", function()
+  window.focusedWindow():moveToUnit'[0,50,50,100]'
+end)
+
+---- left bottom corner
+hotkey.bind({"ctrl","alt"}, "b", function()
+  window.focusedWindow():moveToUnit'[0,50,25,100]'
+end)
+
+---- right bottom corner
+hotkey.bind({"ctrl","alt"}, ",", function()
+  window.focusedWindow():moveToUnit'[75,50,100,100]'
+end)
+
+---- full screen
+--hotkey.bind({"ctrl","alt"}, ";", function()
+  --window.focusedWindow():toggleFullScreen()
 --end)
 
+---- center window
+hotkey.bind({"ctrl","alt"}, "n", function()
+  --window.focusedWindow():centerOnScreen()
+  toggle_center()
+end)
+
+---- defines for window maximize toggler
+local frameCenterCache = {}
+---- toggle a window between its normal size, and being maximized
+function toggle_center()
+    local win = window.focusedWindow()
+    if frameCenterCache[win:id()] then
+        win:setFrame(frameCenterCache[win:id()])
+        frameCenterCache[win:id()] = nil
+    else
+        frameCenterCache[win:id()] = win:frame()
+        --win:centerOnScreen()
+        win:moveToUnit'[25,0,75,100]'
+    end
+end
+
+---- maximize window
+hotkey.bind({"ctrl","alt"}, 'm', function() toggle_maximize() end)
+
+---- defines for window maximize toggler
+local frameCache = {}
+---- toggle a window between its normal size, and being maximized
+function toggle_maximize()
+    local win = window.focusedWindow()
+    if frameCache[win:id()] then
+        win:setFrame(frameCache[win:id()])
+        frameCache[win:id()] = nil
+    else
+        frameCache[win:id()] = win:frame()
+        win:maximize()
+    end
+end
 
 --Predicate that checks if a window belongs to a screen
 function isInScreen(screen, win)
